660967f37636c8495b2aa0c90583a41cb8fd6575
chenguangbo <chenguangbo@juren.com>
2021-01-21 14:35:58 +0800

    2021年1月21日14:35:52  post

--- python_test/http/http_request.py
+++ python_test/http/http_request.py
@@ -12,7 +12,7 @@ kv = json.dumps({"wd": "你好百度", "q": "ww", 'type': '1', 'userId': '123', 
 
 session = req.session()
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-headers = {'Content-Type': 'text/plain', 'User-Agent': user_agent, 'appId': '80',
+headers = {'Content-Type': None, 'User-Agent': user_agent, 'appId': '80',
            'adminToken': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdHVJZCI6Ijg5IiwiZXhwIjoxNjExNzE0OTUyLCJ1c2VySWQiOiI4OSIsImlhdCI6MTYxMTExMDE1Mn0.pJpeGDC_bMHeNFdRRziG62KUX0ha1Y3H9vf8NkMaHV8'}
 # headers = {'Content-Type': 'application/json'}
 try:
